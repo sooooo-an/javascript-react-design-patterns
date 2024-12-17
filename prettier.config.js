31546b262200a1eba04dd8b94ef618598cc2bc75
sooooo-an <devsoo0527@gmail.com>
2024-12-17 13:43:06 +0900

    init: set initial setting

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+
+const config = {
+  semi: false,
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  plugins: ['prettier-plugin-tailwindcss'],
+};
+
+export default config;
